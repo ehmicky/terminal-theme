2576aaa778a624b74f75faf3093e8bbac8e0bae6
ehmicky <ehmicky@gmail.com>
2021-02-28 17:52:57 +0100

    Add test

--- test/main.js
+++ test/main.js
@@ -1,3 +1,5 @@
+import { stderr } from 'process'
+
 import test from 'ava'
 
 import { getCategory, hasStyle } from './helpers/main.js'
@@ -5,4 +7,9 @@ import { getCategory, hasStyle } from './helpers/main.js'
 test('Can specify "colors" option', async (t) => {
   const category = await getCategory({}, { colors: false })
   t.false(hasStyle(category, 'red'))
+})
+
+test('Can specify "stream" option', async (t) => {
+  const category = await getCategory({}, { stream: stderr })
+  t.true(hasStyle(category, 'red'))
 })
