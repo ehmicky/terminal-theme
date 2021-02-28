248781e4c2c8cf5077c193b55bcc059630eec1dc
ehmicky <ehmicky@gmail.com>
2021-02-28 17:49:57 +0100

    Add test helper

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-namespace
+import * as ansiStyles from 'ansi-styles'
+
 import terminalTheme from '../../src/main.js'
 
 export const getCategory = async function (defaultTheme, opts) {
@@ -6,4 +9,8 @@ export const getCategory = async function (defaultTheme, opts) {
     { colors: true, ...opts },
   )
   return category
+}
+
+export const hasStyle = function (category, style) {
+  return category('test').includes(ansiStyles[style].open)
 }
--- test/themer.js
+++ test/themer.js
@@ -1,15 +1,14 @@
-import { modifier, color } from 'ansi-styles'
 import test from 'ava'
 
-import { getCategory } from './helpers/main.js'
+import { getCategory, hasStyle } from './helpers/main.js'
 
 test('Trim style', async (t) => {
   const category = await getCategory({ category: ' red ' })
-  t.true(category('test').includes(color.red.open))
+  t.true(hasStyle(category, 'red'))
 })
 
 test('Can apply multiple styles', async (t) => {
   const category = await getCategory({ category: 'red  bold' })
-  t.true(category('test').includes(color.red.open))
-  t.true(category('test').includes(modifier.bold.open))
+  t.true(hasStyle(category, 'red'))
+  t.true(hasStyle(category, 'bold'))
 })
