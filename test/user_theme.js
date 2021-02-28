9064839fba39b8da5b6626432138026afdac1232
ehmicky <ehmicky@gmail.com>
2021-02-28 17:50:46 +0100

    Add test

--- test/user_theme.js
+++ test/user_theme.js
@@ -3,10 +3,7 @@ import test from 'ava'
 import { getCategory, getCategories, hasStyle } from './helpers/main.js'
 
 test('Use user theme', async (t) => {
-  const category = await getCategory(
-    { category: 'red' },
-    { fixture: 'success' },
-  )
+  const category = await getCategory({}, { fixture: 'success' })
   t.true(hasStyle(category, 'blue'))
 })
 
@@ -16,4 +13,9 @@ test('Shallow merge user theme', async (t) => {
     { fixture: 'success' },
   )
   t.true(hasStyle(otherCategory, 'red'))
+})
+
+test('Allow .yaml extension', async (t) => {
+  const category = await getCategory({}, { fixture: 'yaml' })
+  t.true(hasStyle(category, 'blue'))
 })
