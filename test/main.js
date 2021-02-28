4e57434cd0c2053a31bbaa6bdd8cf3ac73a0ead9
ehmicky <ehmicky@gmail.com>
2021-02-28 15:56:16 +0100

    Start adding tests

--- test/main.js
+++ test/main.js
@@ -1,7 +1,15 @@
 import test from 'ava'
+import hasAnsi from 'has-ansi'
+import { each } from 'test-each'
 
 import terminalTheme from '../src/main.js'
 
-test('Dummy test', (t) => {
-  t.is(typeof terminalTheme, 'function')
+each(['red'], ({ title }, style) => {
+  test(`Can apply any styles | ${title}`, async (t) => {
+    const { category } = await terminalTheme(
+      { category: style },
+      { colors: true },
+    )
+    t.true(hasAnsi(category('test')))
+  })
 })
