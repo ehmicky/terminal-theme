3d699491bedd5d141054cc027cf7f94aaf568c14
ehmicky <ehmicky@gmail.com>
2022-10-09 19:49:18 +0200

    Refactor tests

--- test/methods.js
+++ test/methods.js
@@ -1,47 +1,41 @@
+import ansiStyles from 'ansi-styles'
 import test from 'ava'
 
-import { getCategory, hasStyle } from './helpers/main.js'
+// eslint-disable-next-line no-restricted-imports
+import { chalkString } from '../src/methods.js'
 
-test('Trim style', async (t) => {
-  const category = await getCategory({ category: ' red ' })
-  t.true(hasStyle(category, 'red'))
+const hasStyle = function (string, style) {
+  return string.includes(ansiStyles[style].open)
+}
+
+const chalk = chalkString({ colors: true })
+
+test('Trim style', (t) => {
+  t.true(hasStyle(chalk(' red ', 'test'), 'red'))
 })
 
-test('Can apply multiple styles', async (t) => {
-  const category = await getCategory({ category: 'red  bold' })
-  t.true(hasStyle(category, 'red'))
-  t.true(hasStyle(category, 'bold'))
+test('Can apply multiple styles', (t) => {
+  const string = chalk('red bold', 'test')
+  t.true(hasStyle(string, 'red'))
+  t.true(hasStyle(string, 'bold'))
 })
 
-test('Does not allow non-existing styles', async (t) => {
-  await t.throwsAsync(getCategory({ category: 'doesNotExist' }), {
-    message: /is unknown/u,
-  })
+test('Does not allow non-existing styles', (t) => {
+  t.throws(() => chalk('doesNotExist', 'test'), { message: /is unknown/u })
 })
 
-test('Does not allow non-string styles', async (t) => {
-  await t.throwsAsync(getCategory({ category: true }), {
-    message: /must be a string/u,
-  })
+test('Does not allow non-string styles', (t) => {
+  t.throws(() => chalk(true, 'test'), { message: /must be a string/u })
 })
 
-test('Does not allow arguments with some styles', async (t) => {
-  await t.throwsAsync(getCategory({ category: 'red-255' }), {
-    message: /No arguments/u,
-  })
+test('Does not allow arguments with some styles', (t) => {
+  t.throws(() => chalk('red-255', 'test'), { message: /No arguments/u })
 })
 
-test('Ignores multiple arguments', async (t) => {
-  const category = await getCategory()
-  t.false(category('one', 'two').includes('two'))
+test('Ignores multiple arguments', (t) => {
+  t.false(chalk('red', 'one', 'two').includes('two'))
 })
 
-test('Does not allow non-string arguments', async (t) => {
-  const category = await getCategory()
-  t.throws(() => category(), { message: /Argument must be a string/u })
-})
-
-test('Does not allow chaining', async (t) => {
-  const category = await getCategory()
-  t.throws(() => category.bold('test'), { message: /is not a function/u })
+test('Does not allow non-string arguments', (t) => {
+  t.throws(() => chalk('red'), { message: /Argument must be a string/u })
 })
