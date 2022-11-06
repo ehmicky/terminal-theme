5613911b7b64ab6df59a7a3f29d49945eff9c10f
ehmicky <ehmicky@gmail.com>
2022-11-06 23:53:07 +0100

    Use node protocol

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises'
+import { readFile } from 'node:fs/promises'
 
 import { excludeKeys } from 'filter-obj'
 import { findUp } from 'find-up'
--- test/helpers/main.js
+++ test/helpers/main.js
@@ -1,4 +1,4 @@
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'node:url'
 
 import ansiStyles from 'ansi-styles'
 import terminalTheme from 'terminal-theme'
